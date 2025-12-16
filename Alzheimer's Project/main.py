d768ad40b874edb51ffbdc945839968aa65088ab
porterjurica <porter.jurica@gmail.com>
2025-12-16 15:31:49 -0500

    Add files via upload

---
+++ Alzheimer's Project/main.py
@@ -0,0 +1,7 @@
+# conda install -c conda-forge termcolor
+from patient import Patient
+
+Patient.instantiate_from_csv('UpdatedLuminex.csv', 'UpdatedMetaData.csv')
+
+for patient in Patient.all_patients:
+    print(patient)
